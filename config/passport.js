3cfb15338d5c9996711ff1fca08a7aa8d5753084
aman <bhandari.aman0101@gmail.com>
2022-12-02 20:09:01 +0530

    brute prevent added

--- config/passport.js
+++ config/passport.js
@@ -4,36 +4,74 @@ const bcrypt = require('bcryptjs');
 // Load User model
 const User = require('../models/User');
 
-module.exports = function(passport) {
+module.exports = function (passport) {
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
       // Match user
       User.findOne({
-        email: email
-      }).then(user => {
+        email: email,
+      }).then((user) => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered' });
         }
 
+        if (user.isLocked) {
+          return user.incrementLoginAttempts(function (err) {
+            if (err) {
+              return done(err);
+            }
+            let timestamp = Number(user.lockUntil);
+            console.log(timestamp);
+            let dateFormat = new Date(timestamp);
+            let time =
+              '' +
+              dateFormat.getDate() +
+              '/' +
+              (dateFormat.getMonth() + 1) +
+              '/' +
+              dateFormat.getFullYear() +
+              ' ' +
+              dateFormat.getHours() +
+              ':' +
+              dateFormat.getMinutes() +
+              ':' +
+              dateFormat.getSeconds();
+            return done(null, false, {
+              message: `You have exceeded the maximum number of login attempts.Your account is locked until ${time}`,
+            });
+          });
+        }
+
         // Match password
         bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
           if (isMatch) {
-            return done(null, user);
+            // here need to reset login attempts
+            user.resetAttempts(function (err) {
+              if (err) return done(err);
+              return done(null, user);
+            });
           } else {
-            return done(null, false, { message: 'Password incorrect' });
+            user.incrementLoginAttempts(function (err) {
+              if (err) {
+                return done(err);
+              }
+
+              return done(null, false, {
+                message: `Invalid Password`,
+              });
+            });
           }
         });
       });
     })
   );
 
-  passport.serializeUser(function(user, done) {
+  passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
       done(err, user);
     });
   });
--- models/User.js
+++ models/User.js
@@ -3,21 +3,68 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  loginAttempts: { type: Number, required: true, default: 0 },
+  lockUntil: String,
 });
+
+UserSchema.virtual('isLocked').get(function () {
+  return this.lockUntil && this.lockUntil > Date.now();
+});
+
+UserSchema.methods.resetAttempts = function (callback) {
+  console.log('reset');
+  return this.update(
+    {
+      $set: { loginAttempts: 0 },
+      $unset: { lockUntil: 1 },
+    },
+    callback
+  );
+};
+
+UserSchema.methods.incrementLoginAttempts = function (callback) {
+  console.log('lockUntil', this.lockUntil);
+  // if we have a previous lock that has expired, restart at 1
+  var lockExpired = this.lockUntil && this.lockUntil < Date.now();
+  console.log('lockExpired', lockExpired);
+  if (lockExpired) {
+    return this.update(
+      {
+        $set: { loginAttempts: 0 },
+        $unset: { lockUntil: 1 },
+      },
+      callback
+    );
+  }
+  // otherwise we're incrementing
+  var updates = { $inc: { loginAttempts: 1 } };
+  // lock the account if we've reached max attempts and it's not locked already
+  var needToLock = !!(this.loginAttempts + 1 >= 2 && !this.isLocked);
+  console.log('needToLock', needToLock);
+  console.log('loginAttempts', this.loginAttempts);
+  if (needToLock) {
+    let time = Date.now() + 86400000;
+    time = '' + time;
+    updates.$set = { lockUntil: time };
+    console.log('lockoutHours', time, Date.now());
+  }
+  //console.log("lockUntil",this.lockUntil)
+  return this.update(updates, callback);
+};
 
 const User = mongoose.model('User', UserSchema);
 
